733dafceb2d706e70cf14141e31515a67a462130
jaylindsey <jlindsey@innovodynamics.com>
2018-03-30 17:28:20 -0500

    First Publish

---
+++ PauperLegal/obj/Release/Package/PackageTmp/Scripts/Custom/cardCarousel.js
@@ -0,0 +1,34 @@
+﻿$('.responsive').slick({
+    dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+          }
+      }
+    ]
+});
